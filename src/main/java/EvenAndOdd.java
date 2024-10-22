fd5f3afe1e0c7b76f3a0d180fdfc448be9479701
Kang <linhmiumoon@gmail.com>
2024-10-22 09:34:25 +0700

    1.2

---
+++ src/main/java/EvenAndOdd.java
@@ -0,0 +1,17 @@
+public class EvenAndOdd {
+    public static void main(String[] args) {
+
+        int number = 10;
+
+        if (number % 2 == 0) {
+
+            System.out.println(number + " là số chẵn.");
+
+        } else {
+
+            System.out.println(number + " là số lẻ.");
+
+        }
+
+    }
+}
