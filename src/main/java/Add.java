8728c8360db7114cf78fa53de1b99bf4140c982e
Kang <linhmiumoon@gmail.com>
2024-10-22 09:18:59 +0700

    1.1

---
+++ src/main/java/Add.java
@@ -0,0 +1,12 @@
+public class Add {
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        int b = 3;
+
+        int add = a + b;
+
+        System.out.println("Tổng của hai số là: " + add);
+    }
+}
