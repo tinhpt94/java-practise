2e0e1262f09d8b714add890d8502739a2e2caff0
Kang <linhmiumoon@gmail.com>
2024-10-22 20:31:26 +0700

    Complete 1.1

--- src/main/java/Add.java
+++ src/main/java/Add.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
-
 public class Add {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
 
-        System.out.println("Sum of two number: " + (a+b));
+        int a = 5;
+
+        int b = 3;
+
+        int add = a + b;
+
+        System.out.println("Tổng của hai số là: " + add);
     }
 }
