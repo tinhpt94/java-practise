43ce658147cffa56880c101797c6599b99c032ad
Kang <linhmiumoon@gmail.com>
2024-11-28 10:23:56 +0700

    Complete 2.1 Book

--- src/main/OOP/Book1.java
+++ src/main/OOP/Book1.java
@@ -1,18 +1,20 @@
+import java.util.Arrays;
+
 public class Book1 {
     private String name;
-    private Author author;
+    private Author[] authors;
     private double price;
     private int qty;
-    public Book1(String name, Author author, double price) {
+    public Book1(String name, Author[] authors, double price) {
         this.name = name;
-        this.author = author;
+        this.authors = authors;
         this.price = price;
         qty = 0;
     }
 
-    public Book1(String name, Author author, double price, int qty) {
+    public Book1(String name, Author[] authors, double price, int qty) {
         this.name = name;
-        this.author = author;
+        this.authors = authors;
         this.price = price;
         this.qty = qty;
     }
@@ -21,8 +23,8 @@ public class Book1 {
         return name;
     }
 
-    public Author getAuthor() {
-        return author;
+    public Author[] getAuthor() {
+        return authors;
     }
 
     public void setPrice(double price) {
@@ -43,6 +45,17 @@ public class Book1 {
 
     @Override
     public String toString() {
-        return "Book[name=" + name + "," + author + ",price=" + price + ",qty=" + qty + "]";
+        return "Book[name=" + name + "," + "{" + Arrays.toString(authors) + "}" + ",price=" + price + ",qty=" + qty + "]";
+    }
+
+    public String getAuthorNames() {
+        StringBuilder authorNames = new StringBuilder();
+        for (int i=0; i<authors.length; i++) {
+            authorNames.append(authors[i].getName());
+            if(i < authors.length-1) {
+                authorNames.append(",");
+            }
+        }
+        return authorNames.toString();
     }
 }
--- src/main/OOP/TestBook1.java
+++ src/main/OOP/TestBook1.java
@@ -1,21 +1,20 @@
 public class TestBook1 {
     public static void main(String[] args) {
-        Author ahTeck = new Author("Tan Ah Teck", "ahteck@nowhere.com", 'm');
-        System.out.println(ahTeck);   // Sách toString() của tác giả
+        Author author1 = new Author("Author1", "author1@example.com", 'm');
+        Author author2 = new Author("Author2", "author2@example.com", 'f');
+        System.out.println(author1);
+        System.out.println(author2);
 
-        Book1 dummyBook = new Book1("Java for dummy", ahTeck, 19.95, 99);   // Hàm tạo của Sách kiểm tra
-        System.out.println(dummyBook);   // toString() của Sách kiểm tra
+        Author[] authors = {author1, author2};
 
-        dummyBook.setPrice(29.95);
-        dummyBook.setQty(28);
-        System.out.println("tên là: " + dummyBook.getName());
-        System.out.println("giá là: " + dummyBook.getPrice());
-        System.out.println("số lượng là: " + dummyBook.getQty());
-        System.out.println("Tác giả là: " + dummyBook.getAuthor());   // Author's toString()
-        System.out.println("Tên tác giả là: " + dummyBook.getAuthor().getName());
-        System.out.println("Email của tác giả là: " + dummyBook.getAuthor().getEmail()); // Sử dụng một thể hiện ẩn danh của Author để xây dựng một thể hiện Book
-        Book1 anotherBook = new Book1("more Java",
-                new Author("Paul Tan", "paul@somewhere.com", 'm'), 29.95);
-        System.out.println(anotherBook);   // toString()
+        Book1 book = new Book1("Java for dummy", authors, 19.95, 99);
+        System.out.println(book);
+
+        book.setPrice(29.95);
+        book.setQty(28);
+        System.out.println("Name is: " + book.getName());
+        System.out.println("Price is: " + book.getPrice());
+        System.out.println("Qty is: " + book.getQty());
+        System.out.println("Names of authors is: " + book.getAuthorNames());
     }
 }
