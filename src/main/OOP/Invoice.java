9400f36f577b08d1505e92fa3dea818016fa1ff7
Kang <linhmiumoon@gmail.com>
2024-12-03 11:37:33 +0700

    Complete 2.4 Invoice

---
+++ src/main/OOP/Invoice.java
@@ -0,0 +1,51 @@
+public class Invoice {
+    private int id;
+    private Customer customer;
+    private double amount;
+    public Invoice(int id, Customer customer, double amount) {
+        this.id = id;
+        this.customer = customer;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public int getCustomerId() {
+        return customer.getId();
+    }
+
+    public String getCustomerName() {
+        return customer.getName();
+    }
+
+    public int getCustomerDiscount() {
+        return customer.getDiscount();
+    }
+
+    public double getAmountAfterDiscount() {
+        return amount - (amount * getCustomerDiscount()/100);
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice[id=" + id + ", customer=" + customer + ",amount=" + amount + "]";
+    }
+}
