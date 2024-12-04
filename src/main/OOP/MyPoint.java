6c259409cd75d090806f49cc32c8f46f73a9e868
Kang <linhmiumoon@gmail.com>
2024-12-05 01:23:57 +0700

    Complete 2.6 MyPoint

---
+++ src/main/OOP/MyPoint.java
@@ -0,0 +1,62 @@
+public class MyPoint {
+    private int x;
+    private int y;
+    public MyPoint(){
+        x = 0;
+        y = 0;
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int[] getXY() {
+        return new int[] {x,y};
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
+    public double distance(int x, int y) {
+        int xDiff = this.x - x;
+        int yDiff = this.y - y;
+        return Math.sqrt(xDiff*xDiff + yDiff*yDiff);
+    }
+
+    public double distance(MyPoint another) {
+        int xDiff = this.x - another.x;
+        int yDiff = this.y - another.y;
+        return Math.sqrt(xDiff*xDiff + yDiff*yDiff);
+    }
+
+    public double distance() {
+        int xDiff = this.x;
+        int yDiff = this.y;
+        return Math.sqrt(xDiff*xDiff + yDiff*yDiff);
+    }
+
+}
